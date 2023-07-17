2ad2c39196f21356a1282615cf79f7e55ed993a6
Josepinraj <139473393+Josepinraj@users.noreply.github.com>
2023-07-17 21:01:10 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -7,20 +7,20 @@ TEST_CASE("infers the breach according to limits")
 {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
   REQUIRE(inferBreach(31, 20, 30) == TOO_HIGH);
+  REQUIRE(inferBreach(0, 0, 0) == NORMAL);
 }
 
 TEST_CASE("classify Temperature Breach")
 {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 36) == TOO_HIGH);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46) == TOO_HIGH);
-//  REQUIRE(classifyTemperatureBreach(3, 41) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 41) == TOO_HIGH);
 }
 
 TEST_CASE("check And Alert")
 {
   checkAndAlert(TO_CONTROLLER, {HI_ACTIVE_COOLING, 0}, 46);
   checkAndAlert(TO_EMAIL, {HI_ACTIVE_COOLING, 0}, 46);
-//  REQUIRE(checkAndAlert(2, HI_ACTIVE_COOLING, 0, 46));
 }
 
 TEST_CASE("send To Controller")
@@ -32,6 +32,5 @@ TEST_CASE("send To Email")
 {
   sendToEmail(TOO_HIGH);
   sendToEmail(TOO_LOW);
-//  sendToEmail(3);
 }
 
